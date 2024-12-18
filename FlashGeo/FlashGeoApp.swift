193863729b2c6c7b08181075a3e48a235f34dc57
ChristianCiriello <christianciriello@icloud.com>
2024-12-18 17:17:40 +0100

    Initial Commit

---
+++ FlashGeo/FlashGeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashGeoApp.swift
+//  FlashGeo
+//
+//  Created by Christian Ciriello on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashGeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
